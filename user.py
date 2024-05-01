48e80c74986acff2e5ebe765cbffe0c145b07aae
MaksStud <119805355+MaksStud@users.noreply.github.com>
2024-05-01 19:43:47 +0300

    Додано тип для змінної

--- user.py
+++ user.py
@@ -4,6 +4,7 @@ from aiogram.filters.command import Command
 from message_text import User_message_text
 import keyboards as kb
 
+
 async def command_start(message: Message) -> None:
     user_text = User_message_text()
     await message.answer(user_text.hello_user_message(), reply_markup=kb.hello_kb)
