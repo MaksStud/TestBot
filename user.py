e45452c9726de58cd5ac1e20d71e69589a64636c
MaksStud <119805355+MaksStud@users.noreply.github.com>
2024-05-01 20:07:21 +0300

    Винесено user_text за функцію

--- user.py
+++ user.py
@@ -4,15 +4,15 @@ from aiogram.filters.command import Command
 from message_text import User_message_text
 import keyboards as kb
 
+user_text = User_message_text()
+
 
 async def command_start(message: Message) -> None:
-    user_text = User_message_text()
     await message.answer(user_text.hello_user_message(), reply_markup=kb.hello_kb)
 
 
 async def echo(message: Message) -> None:
-    user_text = User_message_text()
-    await message.answer(user_text.eho_user_message(message.text))
+    await message.answer(user_text.echo_user_message(message.text))
 
 
 def register(dp: Dispatcher):
